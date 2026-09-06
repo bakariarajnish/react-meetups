9bfe2ed26e2af95df5304f344751d6f2aad0c53a
bakariarajnish <rajnishbakariadocs@gmail.com>
2026-09-06 21:27:24 +0100

    Update db.js

--- components/common/db.js
+++ components/common/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 let client = null;
 export default async function DBConnection() {
   client = await MongoClient.connect(
-    "mongodb+srv://rajnishbakaria:Airtel$4321@cluster0.0mscsjp.mongodb.net/meetups?appName=Cluster0",
+    "mongodb+srv://test:test@cluster0.0mscsjp.mongodb.net/meetups?appName=Cluster0",
   );
   const db = client.db();
   const meetupCollection = db.collection("meetups");
